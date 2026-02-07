0687c53198bc67c9d779799e6cbd8f33f2a31e7d
bhola-dev58 <bhya23cse@gmail.com>
2026-02-08 04:08:31 +0530

    Remove staff section, add marquee announcement, show navbar in admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ const Layout = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 transition-colors duration-300">
-      {!isAdminRoute && <Navbar />}
+      <Navbar />
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
